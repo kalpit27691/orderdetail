97ebde3047a53b8e137e557b15300e5d51bc9fb6
kalpit <agarwalthekalpit@gmail.com>
2024-09-09 08:25:14 +0530

    modified src/components/Bar.tsx

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -6,13 +6,10 @@ text:string
 
 
 const Bar:React.FC<Barprops>=(props)=>{
-const t=props.text;
 return (
-<div className="flex relative justify-between border rounded-xl gap-2 w-96">
+<div className="flex  justify-between border rounded-xl gap-2 w-96">
 <p className="p-2 text-left">
     {props.text}
-</p>
-<p className="absolute bottom-0 right-0 p-2 bg-[${#t}]">
 </p>
 </div>
 )
